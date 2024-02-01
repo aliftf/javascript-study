340739735f237015e2b662d556f292d5946a260c
Alif T <alift929@gmail.com>
2024-02-01 12:30:18 +0700

    Error Handling

--- Callback Practice/script.js
+++ Callback Practice/script.js
@@ -3,15 +3,31 @@
 const searchButton = document.querySelector('.search-button');
 
 searchButton.addEventListener('click', async function () {
-    const inputKeyword = document.querySelector('.input-keyword');
-    const movies = await getMovies(inputKeyword.value);
-    updateUI(movies);
+    try {
+        const inputKeyword = document.querySelector('.input-keyword');
+        const movies = await getMovies(inputKeyword.value);
+        updateUI(movies);
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
 });
 
 function getMovies(keyword) {
     return fetch('http://www.omdbapi.com/?apikey=dca61bcc&s=' + keyword)
-        .then(response => response.json())
-        .then(response => response.Search);
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(response => {
+            if (response.Response === "False") {
+                throw new Error(response.Error);
+            }
+            return response.Search;
+        
+        });
 }
 
 function updateUI(movies) {
